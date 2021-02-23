4f6ccffd415031673d246679d87e4e70797cf975
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-02-23 07:52:47 -0500

    npm, lodash, commomJS syntax

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
-import "./shoppingCart";
-console.log("Importing module");
+//all modules are implemented in strict mode.
+//import statements are hoisted.
+// import "./shoppingCart.js";
+// console.log("importing module");
+// //----
+import {
+  addToCart,
+  totalPrice as price,
+  totalQuantity,
+} from "./shoppingCart.js";
+addToCart("pencils", 10);
+console.log(price, totalQuantity);
+//----
+// // import * as ShoppingCart from "./shoppingCart.js";
+// // ShoppingCart.addToCart("wine", 10);
+// //----
+// import deCart from "./shoppingCart.js";
+// console.log(deCart);
+//---same usecase using an iife.
+//---used before es-6 modules
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+//   function addToCart(product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(cart);
+//   }
+//   function orderStock(product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   }
+//   return { addToCart, cart, totalPrice, totalQuantity };
+// })();
+// ShoppingCart2.addToCart("apple", 4);
+// ShoppingCart2.addToCart("pizza", 3);
+
+import cloneDeep from "lodash-es";
+const state = {
+  cart: [
+    {
+      product: "bread",
+      quantity: 5,
+    },
+    {
+      product: "pizza",
+      quantity: 2,
+    },
+  ],
+  user: {
+    name: "Gauri",
+  },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeep = cloneDeep(state);
+console.log(stateDeep);
+state.user.name = "satya";
+console.log(stateDeep);
+console.log(state);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,2 +1,24 @@
 //exporting module
-console.log("Exporting module");
+//all top level variables are scoped to the specific module.
+
+console.log("exporting module");
+
+const shippingCost = 10;
+const cart = [];
+
+export function addToCart(product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${product} added to the cart!`);
+}
+
+const totalPrice = 237;
+const totalQuantity = 23;
+export { totalPrice, totalQuantity };
+export default cart;
+
+//---COMMON JS - would not work in the browser. would in Node.
+
+// export. function addToCart(product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${product} added to the cart!`);
+// }
